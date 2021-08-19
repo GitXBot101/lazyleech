47caab43c4a9836265cc0a44c6584c8aa5e47e83
mallumovieleech <88672268+mallumovieleech@users.noreply.github.com>
2021-08-19 23:01:29 +0530

    Update leech.py

--- lazyleech/plugins/leech.py
+++ lazyleech/plugins/leech.py
@@ -152,10 +152,10 @@ async def directdl_cmd(client, message):
         link = reply.text
     if not link:
         await message.reply_text('''Usage:
-- /directdl <i>&lt;Direct URL&gt; | optional custom file name</i>
-- /directdl <i>(as reply to a Direct URL) | optional custom file name</i>
-- /direct <i>&lt;Direct URL&gt; | optional custom file name</i>
-- /direct <i>(as reply to a Direct URL) | optional custom file name</i>
+- /leechvideo <i>&lt;Direct URL&gt; | optional custom file name</i>
+- /leechvideo <i>(as reply to a Direct URL) | optional custom file name</i>
+- /leechvideo <i>&lt;Direct URL&gt; | optional custom file name</i>
+- /leechvideo <i>(as reply to a Direct URL) | optional custom file name</i>
 
 - /zipdirectdl <i>&lt;Direct URL&gt; | optional custom file name</i>
 - /zipdirectdl <i>(as reply to a Direct URL) | optional custom file name</i>
